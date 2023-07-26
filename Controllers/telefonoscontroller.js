2c3c500ee42c285b9f4d425cd6347c8a966892ca
sindy <cindyhernandez425@gmail.com>
2023-07-26 17:39:19 -0600

    controladores y rutas de 3 tablas

---
+++ Controllers/telefonoscontroller.js
@@ -0,0 +1,39 @@
+const conexion = require('../database');
+
+
+//ACTUALIZAR TODO EL CODIGO SIGUIENTE
+//<GUARDAR un REGISTRO
+
+/*exports.guardarcorreos = (req, res) => {
+    const idcorreo = req.body.idcorreo;
+    const idprofesor = req.body.idprofesor;
+    const correo = req.body.correo;
+
+    conexion.query('INSERT INTO correos SET ?', {
+        idcorreo: idcorreo, idprofessor: idprofesor,
+        correo: correo
+    }, (error, results) => {
+        if (error) {
+            console.log(error);
+        } else {
+            res.redirect('/correos');
+        }
+    });
+};
+
+
+//ACTUALIZAR un REGISTRO
+exports.actualizarcorreos = (req, res) => {
+    const idcorreo = req.body.idcorreo;
+    const idprofesor = req.body.idprofesor;
+    const correo = req.body.correo;
+
+    conexion.query('UPDATE correos SET ? WHERE idcorreo =?', [{ idprofesor: idprofesor, correo: correo }, idcorreo], (error, results) => {
+        if (error) {
+            console.log(error);
+        } else {
+            res.redirect('/correos');
+        }
+    });
+};
+*/
